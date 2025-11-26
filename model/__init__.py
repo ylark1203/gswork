916d43b2a6b578a70f3043e54e83c0e13062d291
ylark <2402468287@qq.com>
2025-11-26 17:03:27 +0800

    wo flame coefficient

--- model/__init__.py
+++ model/__init__.py
@@ -1,6 +1,6 @@
 from .gaussian import GaussianModel
-from .binding import BindingModel, FLAMEBindingModel, FuHeadBindingModel
+# from .binding import BindingModel, FLAMEBindingModel, FuHeadBindingModel
 # from .binding_bbw import BindingModel, FLAMEBindingModel, FuHeadBindingModel
-# from .binding_bbw_wo_binding import BindingModel, FLAMEBindingModel, FuHeadBindingModel
+from .binding_bbw_wo_binding import BindingModel, FLAMEBindingModel, FuHeadBindingModel # 只有网格，不用flame参数
 from .reconstruction import Reconstruction
 from .mv_reconstruction import MultiViewReconstruction
