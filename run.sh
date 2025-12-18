ff1af6db4dd3fae675cab573b06df9698ea1586d
ylark <2402468287@qq.com>
2025-12-18 19:20:22 +0800

    2025年12月18日 19点20分

---
+++ run.sh
@@ -0,0 +1,14 @@
+# python train_offline.py --subject biden --work_name reproduction --config config/offline.yaml --preload
+python calculate_metrics.py --subject biden --work_name reproduction --output_dir output/INSTA
+
+# python train_offline.py --subject justin --work_name reproduction --config config/offline.yaml --preload
+python calculate_metrics.py --subject justin --work_name reproduction --output_dir output/INSTA
+
+# python train_offline.py --subject malte_1 --work_name reproduction --config config/offline.yaml --preload
+python calculate_metrics.py --subject malte_1 --work_name reproduction --output_dir output/INSTA
+
+# python train_offline.py --subject nf_01 --work_name reproduction --config config/offline.yaml --preload
+python calculate_metrics.py --subject nf_01 --work_name reproduction --output_dir output/INSTA
+
+# python train_offline.py --subject wojtek_1 --work_name reproduction --config config/offline.yaml --preload
+python calculate_metrics.py --subject wojtek_1 --work_name reproduction --output_dir output/INSTA
