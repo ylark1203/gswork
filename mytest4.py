ad9c5a342477e86692ebbf94ef5a90d2c5e6d48c
ylark <2402468287@qq.com>
2026-01-06 19:32:37 +0800

    2026年1月6日 19点32分

--- mytest4.py
+++ mytest4.py
@@ -1,11 +1,6 @@
 import numpy as np
 import trimesh
 
-data = np.load("/mnt/data/lyl/codes/RGBAvatar/BBW/vertice_and_faces/5083vertices.npz")
-V = data.f.v_template   # 或 data["verts"]
-F = data.f.faces     # 或 data["triangles"]
-
-mesh = trimesh.Trimesh(vertices=V, faces=F, process=False)
-mesh.export("/mnt/data/lyl/codes/RGBAvatar/model/transform/flame5083.ply")  # 默认会输出较标准的 ply
-print("exported mesh.ply")
+a = "0.0833 0.0537 0.0647 0.0722 0.1477 0.1210 0.0992 0.0589"
+print(a.split())
 
