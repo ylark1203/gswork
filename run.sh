ecf4688877a95b7f2d49eb4e3c7a958ce2f799ca
ylark <2402468287@qq.com>
2026-01-06 09:58:24 +0800

    092026年1月6日 09点58分

--- run.sh
+++ run.sh
@@ -1,14 +1,11 @@
 # python train_offline.py --subject biden --work_name reproduction --config config/offline.yaml --preload
-python calculate_metrics.py --subject biden --work_name reproduction --output_dir output/INSTA
+# python calculate_metrics.py --subject subject1 --work_name reproduction --output_dir output/HR
 
 # python train_offline.py --subject justin --work_name reproduction --config config/offline.yaml --preload
-python calculate_metrics.py --subject justin --work_name reproduction --output_dir output/INSTA
+python calculate_metrics.py --subject subject2 --work_name reproduction --output_dir output/HR
 
 # python train_offline.py --subject malte_1 --work_name reproduction --config config/offline.yaml --preload
-python calculate_metrics.py --subject malte_1 --work_name reproduction --output_dir output/INSTA
+python calculate_metrics.py --subject subject3 --work_name reproduction --output_dir output/HR
 
-# python train_offline.py --subject nf_01 --work_name reproduction --config config/offline.yaml --preload
-python calculate_metrics.py --subject nf_01 --work_name reproduction --output_dir output/INSTA
-
-# python train_offline.py --subject wojtek_1 --work_name reproduction --config config/offline.yaml --preload
-python calculate_metrics.py --subject wojtek_1 --work_name reproduction --output_dir output/INSTA
+python train_offline.py --subject subject4 --work_name reproduction --config config/offline_HR.yaml --preload
+python calculate_metrics.py --subject subject4 --work_name reproduction --output_dir output/HR
